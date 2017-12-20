65f03bb1145155c45459f1b9868a9e372095e068
Your Name <you@example.com>
2017-12-20 07:06:40 +0000

    Add api-defaults

--- src/spike_ring/server.clj
+++ src/spike_ring/server.clj
@@ -1,10 +1,12 @@
 (ns spike-ring.server
-  (:require [immutant.web :as web]))
+  (:require [immutant.web :as web]
+            [ring.middleware.defaults :refer :all]))
 
 (defn app [request]
   {:status 200
    :body   "Hello world!"})
 
+;(web/run (wrap-defaults app api-defaults))
 (web/run app)
 
 (web/stop)
