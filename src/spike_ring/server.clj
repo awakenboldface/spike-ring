56d5b5c995d2b2b9c08b815ab82a78cb6aa62e19
Your Name <you@example.com>
2017-12-26 01:30:49 +0000

    Use wrap-json-body

--- src/spike_ring/server.clj
+++ src/spike_ring/server.clj
@@ -1,12 +1,12 @@
 (ns spike-ring.server
   (:require [immutant.web :as web]
             [ring.middleware.defaults :refer :all]
-            [ring.middleware.params :refer :all]
-            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
             [mount.core :refer [defstate]]))
 
 (defn app
   [request]
+  (println (:body request))
   {:status 200
    :body   [{:hello "world!"} {:hello "world!"}]})
 
@@ -14,7 +14,7 @@
 (def start
   (partial web/run
            (-> app
-               wrap-params
+               (wrap-json-body {:keywords? true})
                wrap-json-response)
            {:host "0.0.0.0"}))
 
